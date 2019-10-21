19c52f7bc4b0886b252f3e2e780f816351611839
Sven Giebel <svengiebel@me.com>
2019-10-21 17:37:52 +0200

    add lumen basic server, add basic JWT auth

---
+++ opennotezServer/app/Http/Controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use  App\User;
+
+class AuthController extends Controller
+{
+    /**
+     * Store a new user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function register(Request $request)
+    {
+        //validate incoming request 
+        $this->validate($request, [
+            'username' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        try {
+
+            $user = new User;
+            $user->username = $request->input('username');
+            $user->email = $request->input('email');
+            $plainPassword = $request->input('password');
+            $user->password = app('hash')->make($plainPassword);
+
+            $user->save();
+
+            //return successful response
+            return response()->json(['user' => $user, 'message' => 'CREATED'], 201);
+
+        } catch (\Exception $e) {
+            //return error message
+            return response()->json(['message' => 'User Registration Failed!'], 409);
+        }
+
+    }
+
+    public function login(Request $request)
+    {
+      $this->validate($request, [
+        'email' => 'required|string',
+        'password' => 'required|string'
+      ]);
+
+      $creds = $request->only(['email', 'password']);
+
+      if (! $token = Auth::attempt($creds) ) {
+        return response()->json(['message' => 'Unauthorized'], 401);
+      }
+
+      return $this->respondWithToken($token);
+    }
+
+
+}
