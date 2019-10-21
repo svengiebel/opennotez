1dfb259c81e9fa7b2f11c1f3cd2b5a8c09fb1873
Sven Giebel <svengiebel@me.com>
2019-10-21 17:44:07 +0200

    add route for getting current user profile info

--- opennotezServer/routes/web.php
+++ opennotezServer/routes/web.php
@@ -17,5 +17,7 @@ $router->get('/', function () use ($router) {
 
 $router->group(['prefix' => 'api'], function () use ($router) {
   $router->post('register', 'AuthController@register');
+
   $router->post('login', 'AuthController@login');
+  $router->get('profile', 'UserController@profile');
 });
